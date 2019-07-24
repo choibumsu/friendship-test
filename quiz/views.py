12de8726bc3737a2d7192419eb4279e723a5001c
bumsu <bumsu0211@gmail.com>
2019-07-24 15:11:40 +0900

    modify bug

--- quiz/views.py
+++ quiz/views.py
@@ -25,14 +25,14 @@ def quiz_create(request, pk):
     num = 1
     if request.POST:
         num = int(request.POST['quiz_id']) + 1
-        user.answer = ''.join([user.answer, request.POST['answer']])
-        user.save()
         if num > 10:
             return redirect("quiz:quiz_complete", pk)
-
+        user.answer = ''.join([user.answer, request.POST['answer']])
+        user.save()
+        
     quiz = get_object_or_404(Quiz, id=num)
     
-    return render(request, "quiz/quiz_create.html", {'quiz':quiz, 'num':num})
+    return render(request, "quiz/quiz_create.html", {'quiz':quiz})
 
 def quiz_complete(request, pk):
     user = get_object_or_404(User, pk=pk)
@@ -63,21 +63,21 @@ def solve_home(request, quiz_url):
 
 def solve_quiz(request, pk):
     challenger = get_object_or_404(Challenger, pk=pk)
-    user = get_object_or_404(User, pk=challenger.user_obj.pk)
+    user = challenger.user_obj
 
     num = 1
     if request.POST:
         num = int(request.POST['quiz_id']) + 1
-        if num > 10:
-            return render(request, "quiz/solve_result.html", {"user":user, "challenger":challenger})
-
+        
         if request.POST['answer'] == user.answer[num-2]:
             challenger.result += 1
-            challenger.save()        
+            challenger.save()
+        if num > 10:
+                return render(request, "quiz/solve_result.html", {"user":user, "challenger":challenger})
 
     quiz = get_object_or_404(Quiz, id=num)
     
-    return render(request, "quiz/solve_quiz.html", {'quiz':quiz, 'num':num})
+    return render(request, "quiz/solve_quiz.html", {'quiz':quiz})
 
 def solve_result(request, pk):
     challenger = get_object_or_404(Challenger, pk=pk)
